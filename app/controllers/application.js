5801830ef22bbf6d9c2f678d42fea1dcd7d25369
Ben Hummon <ben.hummon@gmail.com>
2020-10-21 13:08:17 -0500

    Add RNG when choosing midpoint

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -4,14 +4,11 @@ import { action } from '@ember/object';
 
 export default class ApplicationController extends Controller {
   @tracked sad = 0;
-  @tracked happy = 10;
+  @tracked happy = 50;
+  @tracked midpoint = 25;
 
   get amDone() {
     return Math.abs(this.sad - this.happy) <= 1;
-  }
-
-  get midpoint() {
-    return Math.floor((this.sad + this.happy) / 2);
   }
 
   @action addDataPoint(number, isHappy) {
@@ -20,5 +17,18 @@ export default class ApplicationController extends Controller {
     } else {
       this.sad = number;
     }
+    this.chooseMidpoint();
+  }
+
+  chooseMidpoint() {
+    let total = this.sad + this.happy;
+    if (total % 2 === 1) {
+      if (Math.floor(2 * Math.random()) === 0) {
+        total += 1;
+      } else {
+        total -= 1;
+      }
+    }
+    this.midpoint = total / 2;
   }
 }
