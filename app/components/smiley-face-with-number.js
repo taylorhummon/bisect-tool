b9ccf6a071789b10f6b19be088d990d6b781498a
Ben Hummon <ben.hummon@gmail.com>
2020-10-24 10:50:17 -0500

    Writing a departure animation

--- app/components/smiley-face-with-number.js
+++ app/components/smiley-face-with-number.js
@@ -10,11 +10,22 @@ export default Component.extend({
   _onAnimationEndBound: null,
 
   @action smileyFaceClicked() {
-    if (Math.floor(2 * Math.random())) {
-      this._animateCenterToLeft();
-    } else {
-      this._animateCenterToRight();
-    }
+    this._animateDepart();
+
+    // if (Math.floor(2 * Math.random())) {
+    //   this._animateCenterToLeft();
+    // } else {
+    //   this._animateCenterToRight();
+    // }
+  },
+
+  _animateDepart() {
+    const onAnimationEnd = () => {
+      this.element.removeEventListener('animationend', onAnimationEnd);
+      this.set('stateOrTransition', 'state-invisible');
+    };
+    this.element.addEventListener('animationend', onAnimationEnd);
+    this.set('stateOrTransition', 'transition-depart');
   },
 
   _animateCenterToLeft() {
