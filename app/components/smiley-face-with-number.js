cf6f09875797fe5e887b5857017824000428a2fb
Ben Hummon <ben.hummon@gmail.com>
2020-10-26 11:26:50 -0500

    Use transform/translate for CSS animations
    They can be better optimized by the browser over adjusting absolute 
    positioning.

--- app/components/smiley-face-with-number.js
+++ app/components/smiley-face-with-number.js
@@ -1,50 +1,75 @@
 import Component from '@ember/component';
 import { action } from '@ember/object';
 
+function flipCoin() {
+  return Math.floor(2 * Math.random());
+}
+
 // !!! consider making this a class
 export default Component.extend({
   classNames: ['smiley-face-with-number'],
-  classNameBindings: ['stateOrTransition'],
-  stateOrTransition: 'state-center',
-
-  _onAnimationEndBound: null,
+  classNameBindings: ['opacity', 'position'],
+  opacity: 'state-opaque',
+  position: 'state-center',
 
   @action smileyFaceClicked() {
-    this._animateDepart();
-
-    // if (Math.floor(2 * Math.random())) {
-    //   this._animateCenterToLeft();
-    // } else {
-    //   this._animateCenterToRight();
-    // }
+    if (flipCoin()) { // change opacity
+      if (this.opacity === 'state-opaque') {
+        this._animateOpaqueToTransparent();
+      } else {
+        this._animateTransparentToOpaque();
+      }
+    } else { // change position
+      if (this.position === 'state-center') {
+        if (flipCoin()) {
+          this._animateCenterToLeft();
+        } else {
+          this._animateCenterToRight();
+        }
+      } else {
+        console.log('Setting position to center');
+        this.set('position', 'state-center');
+      }
+    }
   },
 
-  _animateDepart() {
+  _animateOpaqueToTransparent() {
+    if (this.opacity !== 'state-opaque') return;
     const onAnimationEnd = () => {
       this.element.removeEventListener('animationend', onAnimationEnd);
-      this.set('stateOrTransition', 'state-invisible');
+      this.set('opacity', 'state-transparent');
     };
     this.element.addEventListener('animationend', onAnimationEnd);
-    this.set('stateOrTransition', 'transition-depart');
+    this.set('opacity', 'transition-opaque-to-transparent');
+  },
+
+  _animateTransparentToOpaque() {
+    if (this.opacity !== 'state-transparent') return;
+    const onAnimationEnd = () => {
+      this.element.removeEventListener('animationend', onAnimationEnd);
+      this.set('opacity', 'state-opaque');
+    };
+    this.element.addEventListener('animationend', onAnimationEnd);
+    this.set('opacity', 'transition-transparent-to-opaque');
   },
 
   _animateCenterToLeft() {
-    if (this.stateOrTransition !== 'state-center') return;
+    if (this.position !== 'state-center') return;
     const onAnimationEnd = () => {
       this.element.removeEventListener('animationend', onAnimationEnd);
-      this.set('stateOrTransition', 'state-left');
+      this.set('position', 'state-left');
     };
     this.element.addEventListener('animationend', onAnimationEnd);
-    this.set('stateOrTransition', 'transition-center-to-left');
+    this.set('position', 'transition-center-to-left');
   },
 
   _animateCenterToRight() {
-    if (this.stateOrTransition !== 'state-center') return;
+    if (this.position !== 'state-center') return;
     const onAnimationEnd = () => {
       this.element.removeEventListener('animationend', onAnimationEnd);
-      this.set('stateOrTransition', 'state-right');
+      this.set('position', 'state-right');
     };
     this.element.addEventListener('animationend', onAnimationEnd);
-    this.set('stateOrTransition', 'transition-center-to-right');
+    this.set('position', 'transition-center-to-right');
   },
 });
