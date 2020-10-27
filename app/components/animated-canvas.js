03d80be50200a08fd16750f050455939195144ac
Ben Hummon <ben.hummon@gmail.com>
2020-10-27 16:17:45 -0500

    Move animations to animated-canvas

--- app/components/animated-canvas.js
+++ app/components/animated-canvas.js
@@ -2,6 +2,10 @@ import Component from '@ember/component';
 import EmberObject from '@ember/object';
 import { action } from '@ember/object';
 
+function flipCoin() {
+  return Math.floor(2 * Math.random());
+}
+
 // !!! consider making this a class
 export default Component.extend({
   smilies: null,
@@ -10,21 +14,94 @@ export default Component.extend({
     console.log('IN INIT');
     this._super(...arguments);
     const initialLeftSmiley = EmberObject.create({
-      position: 'state-left',
+      id: 'id-1',
+      smile: 'state-sad',
       opacity: 'state-opaque',
-      count: 1
+      position: 'state-left',
     });
     const initialCenterSmiley = EmberObject.create({
-      position: 'state-center',
+      id: 'id-2',
+      smile: 'state-happy',
       opacity: 'state-opaque',
-      count: 2
+      position: 'state-center',
     });
     const initialRightSmiley = EmberObject.create({
-      position: 'state-right',
+      id: 'id-3',
+      smile: 'state-happy',
       opacity: 'state-opaque',
-      count: 3
+      position: 'state-right',
     });
     const smilies = [initialLeftSmiley, initialCenterSmiley, initialRightSmiley];
     this.set('smilies', smilies);
+  },
+
+  @action onSmileyClick(smiley) { // !!! does this need to be an action?
+    if (smiley.position !== 'state-center') return;
+    this._removeSmileyByPosition('state-right');
+
+    // make the right smiley disappear and remove it from the list of smilies.
+    // make the center smiley move to the right.
+    // make a new center smiley and make it appear
+  },
+
+  _removeSmileyByPosition(smileyPosition) {
+    const smiley = this.smilies.find(
+      smiley => smiley.position === smileyPosition
+    );
+    if (! smiley) return;
+    const domElement = this.element.querySelector(`.${smiley.id}`);
+    if (! domElement) return;
+    this._animateOpaqueToTransparent(smiley, domElement);
+  },
+
+  _removeSmilies() {
+    for (let i = this.smilies.length - 1; i >= 0; i--) {
+      const smiley = this.smilies.objectAt(i);
+      if (smiley.opacity === 'state-transparent') { // !!!
+        this.smilies.removeAt(i);
+      }
+    }
+  },
+
+  _animateOpaqueToTransparent(smiley, domElement) {
+    if (smiley.opacity !== 'state-opaque') return;
+    const onAnimationEnd = () => { // !!!! maybe make this promisey?
+      domElement.removeEventListener('animationend', onAnimationEnd);
+      smiley.set('opacity', 'state-transparent');
+      this._removeSmilies();
+      console.log('SMILIES', this.smilies);
+    };
+    domElement.addEventListener('animationend', onAnimationEnd);
+    smiley.set('opacity', 'transition-opaque-to-transparent');
+  },
+
+  _animateTransparentToOpaque() {
+    if (this.opacity !== 'state-transparent') return;
+    const onAnimationEnd = () => {
+      this.element.removeEventListener('animationend', onAnimationEnd);
+      this.set('opacity', 'state-opaque');
+    };
+    this.element.addEventListener('animationend', onAnimationEnd);
+    this.set('opacity', 'transition-transparent-to-opaque');
+  },
+
+  _animateCenterToLeft() {
+    if (this.position !== 'state-center') return;
+    const onAnimationEnd = () => {
+      this.element.removeEventListener('animationend', onAnimationEnd);
+      this.set('position', 'state-left');
+    };
+    this.element.addEventListener('animationend', onAnimationEnd);
+    this.set('position', 'transition-center-to-left');
+  },
+
+  _animateCenterToRight() {
+    if (this.position !== 'state-center') return;
+    const onAnimationEnd = () => {
+      this.element.removeEventListener('animationend', onAnimationEnd);
+      this.set('position', 'state-right');
+    };
+    this.element.addEventListener('animationend', onAnimationEnd);
+    this.set('position', 'transition-center-to-right');
   },
 });
--- app/components/smiley-face-with-number.js
+++ app/components/smiley-face-with-number.js
@@ -1,84 +1,22 @@
 import Component from '@ember/component';
 import { action } from '@ember/object';
-import { alias } from '@ember/object/computed';
 import { readOnly } from '@ember/object/computed';
-
-function flipCoin() {
-  return Math.floor(2 * Math.random());
-}
 
 // !!! consider making this a class
 export default Component.extend({
-  classNames: ['smiley-face-with-number'],
-  classNameBindings: ['opacity', 'position'],
-
   smiley: null,
-  smilies: null, // !!! this is gross
+  onSmileyClick: null, // closure action
 
-  opacity: alias('smiley.opacity'),
-  position: alias('smiley.position'),
+  smileId: readOnly('smiley.id'),
+  smile: readOnly('smiley.smile'),
+  opacity: readOnly('smiley.opacity'),
+  position: readOnly('smiley.position'),
 
-  init() {
-    this._super(...arguments);
-    console.log('SMILEY', this.smiley);
-  },
+  classNames: ['smiley-face-with-number'],
+  classNameBindings: ['smileId', 'opacity', 'position'],
 
   @action smileyFaceClicked() {
-    console.log('CLICKED', this.smiley);
-    if (this.position === 'state-center') {
-      this._animateCenterToRight();
-    } else {
-      this._animateOpaqueToTransparent();
-    }
-  },
-
-  _animateOpaqueToTransparent() {
-    if (this.opacity !== 'state-opaque') return;
-    const onAnimationEnd = () => {
-      this.element.removeEventListener('animationend', onAnimationEnd);
-      this.set('opacity', 'state-transparent');
-      this._removeSmilies();
-    };
-    this.element.addEventListener('animationend', onAnimationEnd);
-    this.set('opacity', 'transition-opaque-to-transparent');
-  },
-
-  _removeSmilies() {
-    for (let i = this.smilies.length - 1; i >= 0; i--) {
-      const smiley = this.smilies.objectAt(i);
-      if (smiley.opacity === 'state-transparent') { // !!!
-        this.smilies.removeAt(i);
-      }
-    }
-  },
-
-  _animateTransparentToOpaque() {
-    if (this.opacity !== 'state-transparent') return;
-    const onAnimationEnd = () => {
-      this.element.removeEventListener('animationend', onAnimationEnd);
-      this.set('opacity', 'state-opaque');
-    };
-    this.element.addEventListener('animationend', onAnimationEnd);
-    this.set('opacity', 'transition-transparent-to-opaque');
-  },
-
-  _animateCenterToLeft() {
-    if (this.position !== 'state-center') return;
-    const onAnimationEnd = () => {
-      this.element.removeEventListener('animationend', onAnimationEnd);
-      this.set('position', 'state-left');
-    };
-    this.element.addEventListener('animationend', onAnimationEnd);
-    this.set('position', 'transition-center-to-left');
-  },
-
-  _animateCenterToRight() {
-    if (this.position !== 'state-center') return;
-    const onAnimationEnd = () => {
-      this.element.removeEventListener('animationend', onAnimationEnd);
-      this.set('position', 'state-right');
-    };
-    this.element.addEventListener('animationend', onAnimationEnd);
-    this.set('position', 'transition-center-to-right');
+    if (! this.onSmileyClick) return;
+    this.onSmileyClick(this.smiley);
   },
 });
