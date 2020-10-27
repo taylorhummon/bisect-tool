22096e1269753c704ec8087dd64e3244e573f974
Ben Hummon <ben.hummon@gmail.com>
2020-10-27 16:45:24 -0500

    Composing animations

--- app/components/animated-canvas.js
+++ app/components/animated-canvas.js
@@ -36,10 +36,10 @@ export default Component.extend({
     this.set('smilies', smilies);
   },
 
-  @action onSmileyClick(smiley) { // !!! does this need to be an action?
+  @action async onSmileyClick(smiley) { // !!! does this need to be an action?
     if (smiley.position !== 'state-center') return;
-    this._removeSmileyByPosition('state-right'); // !!! should await
-
+    await this._removeSmileyByPosition('state-right');
+    await this._moveSmileToRight();
     // make the right smiley disappear and remove it from the list of smilies.
     // make the center smiley move to the right.
     // make a new center smiley and make it appear
@@ -78,6 +78,29 @@ export default Component.extend({
     });
   },
 
+  async _moveSmileToRight() {
+    const smiley = this.smilies.find(
+      smiley => smiley.position === 'state-center'
+    );
+    if (! smiley) return;
+    const domElement = this.element.querySelector(`.${smiley.id}`);
+    if (! domElement) return;
+    await this._animateCenterToRight(smiley, domElement);
+  },
+
+  _animateCenterToRight(smiley, domElement) {
+    if (smiley.position !== 'state-center') return RSVP.reject('Smiley must be centered in order to transition from center to right');
+    return new RSVP.Promise((resolve, reject) => {
+      const onAnimationEnd = () => {
+        domElement.removeEventListener('animationend', onAnimationEnd);
+        smiley.set('position', 'state-right');
+        resolve();
+      };
+      domElement.addEventListener('animationend', onAnimationEnd);
+      smiley.set('position', 'transition-center-to-right');
+    });
+  },
+
   _animateTransparentToOpaque() {
     if (this.opacity !== 'state-transparent') return;
     const onAnimationEnd = () => {
@@ -96,15 +119,5 @@ export default Component.extend({
     };
     this.element.addEventListener('animationend', onAnimationEnd);
     this.set('position', 'transition-center-to-left');
-  },
-
-  _animateCenterToRight() {
-    if (this.position !== 'state-center') return;
-    const onAnimationEnd = () => {
-      this.element.removeEventListener('animationend', onAnimationEnd);
-      this.set('position', 'state-right');
-    };
-    this.element.addEventListener('animationend', onAnimationEnd);
-    this.set('position', 'transition-center-to-right');
   },
 });
