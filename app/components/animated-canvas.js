0d9cb538e1a4d4a689a879df617154c8eeb03aca
Ben Hummon <ben.hummon@gmail.com>
2020-10-28 17:54:03 -0500

    DRY animate function

--- app/components/animated-canvas.js
+++ app/components/animated-canvas.js
@@ -27,12 +27,19 @@ export default Component.extend({
     await this._createCenterSmiley();
   },
 
+  async _createCenterSmiley() {
+    const smiley = this._buildSmiley('state-happy', 'state-transparent', 'state-center');
+    this.smilies.pushObject(smiley);
+    await this.utils.delayPromise(0); // !!! ugh
+    await this._animate(smiley, 'opacity', 'transparent', 'opaque');
+  },
+
   async _removeSmileyByPosition(smileyPosition) {
     const smiley = this.smilies.find(
       smiley => smiley.position === smileyPosition
     );
     if (! smiley) return;
-    await this._animateOpaqueToTransparent(smiley);
+    await this._animate(smiley, 'opacity', 'opaque', 'transparent');
     this._removeSmilies();
   },
 
@@ -45,77 +52,27 @@ export default Component.extend({
     }
   },
 
-  _animateOpaqueToTransparent(smiley) {
-    if (smiley.opacity !== 'state-opaque') return RSVP.reject('Smiley must be opaque in order to transition from opaque to transparent');
-    const domElement = this._domElementForSmiley(smiley);
-    if (! domElement) return RSVP.reject('Could not find dom element');
-    return new RSVP.Promise(resolve => {
-      const onAnimationEnd = () => {
-        domElement.removeEventListener('animationend', onAnimationEnd);
-        smiley.set('opacity', 'state-transparent');
-        resolve();
-      };
-      domElement.addEventListener('animationend', onAnimationEnd);
-      smiley.set('opacity', 'transition-opaque-to-transparent');
-    });
-  },
-
   async _moveSmileyToRight() {
     const smiley = this.smilies.find(
       smiley => smiley.position === 'state-center'
     );
     if (! smiley) return;
-    await this._animateCenterToRight(smiley);
+    await this._animate(smiley, 'position', 'center', 'right');
   },
 
-  _animateCenterToRight(smiley) {
-    if (smiley.position !== 'state-center') return RSVP.reject('Smiley must be centered in order to transition from center to right');
-    const domElement = this._domElementForSmiley(smiley);
-    if (! domElement) return RSVP.reject('Could not find dom element');
-    return new RSVP.Promise(resolve => {
-      const onAnimationEnd = () => {
-        domElement.removeEventListener('animationend', onAnimationEnd);
-        smiley.set('position', 'state-right');
-        resolve();
-      };
-      domElement.addEventListener('animationend', onAnimationEnd);
-      smiley.set('position', 'transition-center-to-right');
-    });
-  },
-
-  async _createCenterSmiley() {
-    const smiley = this._buildSmiley('state-happy', 'state-transparent', 'state-center');
-    this.smilies.pushObject(smiley);
-    await this.utils.delayPromise(0); // !!! ugh
-    this._animateTransparentToOpaque(smiley);
-  },
-
-  _animateTransparentToOpaque(smiley) {
-    if (smiley.opacity !== 'state-transparent') return RSVP.reject('Smiley must be transparent in order to transition from transparent to opaque');
-    const domElement = this._domElementForSmiley(smiley);
-    if (! domElement) return RSVP.reject('Could not find dom element');
-    return new RSVP.Promise(resolve => {
-      const onAnimationEnd = () => {
-        domElement.removeEventListener('animationend', onAnimationEnd);
-        smiley.set('opacity', 'state-opaque');
-        resolve();
-      };
-      domElement.addEventListener('animationend', onAnimationEnd);
-      smiley.set('opacity', 'transition-transparent-to-opaque');
-    });
-  },
-
-  _animateCenterToLeft(smiley) {
-    if (smiley.position !== 'state-center') return RSVP.reject('Smiley must be centered in order to transition from center to left');
+  _animate(smiley, attribute, from, to) {
+    if (smiley[attribute] !== `state-${from}`) {
+      return RSVP.reject(`Smiley must have ${attribute} be equal to state-${from} in order to transition to state-${to}`);
+    }
     const domElement = this._domElementForSmiley(smiley);
     return new RSVP.Promise(resolve => {
       const onAnimationEnd = () => {
         domElement.removeEventListener('animationend', onAnimationEnd);
-        smiley.set('position', 'state-left');
+        smiley.set(attribute, `state-${to}`);
         resolve();
       };
       domElement.addEventListener('animationend', onAnimationEnd);
-      smiley.set('position', 'transition-center-to-left');
+      smiley.set(attribute, `transition-from-${from}-to-${to}`);
     });
   },
 
