eee980564a01f2fbd70c0c455e182bf06b820d39
Ben Hummon <ben.hummon@gmail.com>
2020-11-08 13:29:32 -0600

    Creating intro-gather-inputs component

---
+++ app/components/intro-gather-inputs.js
@@ -0,0 +1,8 @@
+import Component from '@ember/component';
+
+export default Component.extend({
+  happyValueString: null,
+  sadValueString: null,
+
+  showError: false,
+});
--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,46 +1,36 @@
 import Controller from '@ember/controller';
 import { tracked } from '@glimmer/tracking';
-import { action } from '@ember/object';
+// import { action } from '@ember/object';
 
 export default class ApplicationController extends Controller {
-  @tracked sad = 0;
-  @tracked happy = 50;
-  @tracked midpoint = 25;
+  // @tracked sad = 0;
+  // @tracked happy = 50;
+  // @tracked midpoint = 25;
 
-  get amDone() {
-    return Math.abs(this.sad - this.happy) <= 1;
-  }
+  @tracked inIntro = true;
 
-  @action addDataPoint(number, isHappy) {
-    if (isHappy) {
-      this.happy = number;
-    } else {
-      this.sad = number;
-    }
-    this.chooseMidpoint();
-  }
-
-  chooseMidpoint() {
-    let total = this.sad + this.happy;
-    if (total % 2 === 1) {
-      if (Math.floor(2 * Math.random()) === 0) {
-        total += 1;
-      } else {
-        total -= 1;
-      }
-    }
-    this.midpoint = total / 2;
-  }
-
-  // @action startAnimation() {
-  //   console.log('STARTING ANIMATION');
-  //   const element = document.getElementById("watchme");
-  //   element.addEventListener("animationend", this.listener, false);
-  //   element.className = "slidein";
+  // get amDone() {
+  //   return Math.abs(this.sad - this.happy) <= 1;
   // }
   //
-  // listener() {
-  //   const element = document.getElementById("watchme");
-  //   element.className = "stationary";
+  // @action addDataPoint(number, isHappy) {
+  //   if (isHappy) {
+  //     this.happy = number;
+  //   } else {
+  //     this.sad = number;
+  //   }
+  //   this.chooseMidpoint();
+  // }
+  //
+  // chooseMidpoint() {
+  //   let total = this.sad + this.happy;
+  //   if (total % 2 === 1) {
+  //     if (Math.floor(2 * Math.random()) === 0) {
+  //       total += 1;
+  //     } else {
+  //       total -= 1;
+  //     }
+  //   }
+  //   this.midpoint = total / 2;
   // }
 }
