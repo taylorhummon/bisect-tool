2042d0f2fd0bcf194cc7628b5cd6f06aa487b646
Ben Hummon <ben.hummon@gmail.com>
2020-11-13 12:37:10 -0600

    Simplify registry service

--- app/services/registry.js
+++ app/services/registry.js
@@ -1,57 +1,40 @@
 import Service from '@ember/service';
 
 export default Service.extend({
-  _groupingComponents: null,
-  _faceComponents: null,
-  _questionComponents: null,
+  _animatedComponents: null,
 
   init() {
     this._super(...arguments);
-    this.set('_groupingComponents', new Map());
-    this.set('_faceComponents', new Map());
-    this.set('_questionComponents', new Map());
+    this.set('_animatedComponents', new Map());
   },
 
   registerComponent(type, id, component) {
+    if (! type) throw 'Missing type';
     if (! id) throw 'Missing id';
     if (! component) throw 'Missing component';
-    if (type === 'grouping') {
-      this._groupingComponents.set(id, component);
-    } else if (type === 'face') {
-      this._faceComponents.set(id, component);
-    } else if (type === 'question') {
-      this._questionComponents.set(id, component);
-    } else {
-      throw `Unknown type: ${type}`;
-    }
+    const key = this._key(type, id);
+    this._animatedComponents.set(key, component);
   },
 
   unregisterComponent(type, id) {
+    if (! type) throw 'Missing type';
     if (! id) throw 'Missing id';
-    if (type === 'grouping') {
-      this._groupingComponents.delete(id);
-    } else if (type === 'face') {
-      this._faceComponents.delete(id);
-    } else if (type === 'question') {
-      this._questionComponents.delete(id);
-    } else {
-      throw `Unknown type: ${type}`;
-    }
+    const key = this._key(type, id);
+    this._animatedComponents.delete(key);
   },
 
   componentFor(object) {
-    if (object.type === 'grouping') {
-      const found = this._groupingComponents.get(object.id);
-      if (found) return found;
-    }
-    if (object.type === 'face') {
-      const found = this._faceComponents.get(object.id);
-      if (found) return found;
-    }
-    if (object.type === 'question') {
-      const found = this._questionComponents.get(object.id);
-      if (found) return found;
-    }
-    throw `Could not find component for object ${object}`;
+    const type = object.type;
+    const id = object.id;
+    if (! type) throw 'Missing type';
+    if (! id) throw 'Missing id';
+    const key = this._key(type, id);
+    const animatedComponent = this._animatedComponents.get(key);
+    if (! animatedComponent) throw `Could not find component for ${type} with id=${id}`;
+    return animatedComponent;
+  },
+
+  _key(type, id) {
+    return `${type}-${id}`;
   },
 });
